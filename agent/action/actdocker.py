749f99268051089fa5963650647984b0f8b88727
Abishek Pechiappan <108849826+Abishek-Pechiappan@users.noreply.github.com>
2026-07-09 08:27:28 -0700

    Updated better code

--- agent/action/actdocker.py
+++ agent/action/actdocker.py
@@ -1,22 +1,30 @@
 import subprocess
 import docker
 
-def docker_down():         # You can down the container by the name of it 
-    client = docker.from_env()
-    up = client.containers.list()
-    for ids in up:
-        print(ids.name)
-    kill = input('container you want to kill :')
-    for ids in up:
-        if kill == ids.name:
-            ids.kill()
+client = docker.from_env()
+
+def containers_stopped():
+    return client.containers.list(filters={'status': 'exited'})
             
-def docker_up():            # You can start an conatiner by the name of it
-    client = docker.from_env()
-    down = client.containers.list(filters={'status': 'exited'})
-    for ids in down:
-        print(ids.name)
-    start = input('Enter the container Name: ')
-    for ids in down:
-        if ids.name == start:
-            ids.start()
+def containers_running():
+    return client.containers.list()
+
+def docker_down():         # You can down the container by the name of it 
+    running = containers_running()
+    for container in running:
+        print(container.name)
+    name = input('container you want to kill :')
+    for container in running:
+        if container.name == name:
+            container.kill()
+            
+def docker_up():
+    stopped = containers_stopped()   
+    for container in stopped:
+        print(container.name)
+    name = input('Enter the container Name: ')
+    for container in stopped:
+        if container.name == name:
+            container.start()
+    
+docker_down()
